cbb42562cca393e38c50c53e58e63f831c76b894
Tyleryin <601576661@qq.com>
2022-03-29 21:49:23 +0800

    finish week's chapter 6

--- texture.h
+++ texture.h
@@ -2,7 +2,8 @@
 #define TEXTURE_H
 //纹理                                                                                                                                                                                                                                                                                                                                                                                               
 #include "rtweekend.h"
-
+#include "perlin.h"
+#include "rtw_stb_image.h"
 class texture {
     public:
         virtual color value(double u, double v, const point3& p) const = 0;//(u=0,v=0)  maps to the bottom-left corner of the texture
@@ -45,5 +46,74 @@ class checker_texture : public texture {
     public:
         shared_ptr<texture> odd;
         shared_ptr<texture> even;
+};
+
+class noise_texture : public texture {
+    public:
+        noise_texture() {}
+        noise_texture(double sc=1) : scale(sc) {}
+
+        virtual color value(double u, double v, const point3& p) const override {
+            // return color(1,1,1) * 0.5 * (1.0 + noise.noise(scale * p));
+            return color(1,1,1) * 0.5 * (1 + sin(scale*p.z() + 10*noise.turb(p)));
+        }
+
+    public:
+        perlin noise;
+        double scale;
+};
+
+//图像纹理
+class image_texture : public texture {
+    public:
+        const static int bytes_per_pixel = 3;
+
+        image_texture()
+          : data(nullptr), width(0), height(0), bytes_per_scanline(0) {}
+
+        image_texture(const char* filename) {
+            auto components_per_pixel = bytes_per_pixel;
+
+            data = stbi_load(
+                filename, &width, &height, &components_per_pixel, components_per_pixel);
+
+            if (!data) {
+                std::cerr << "ERROR: Could not load texture image file '" << filename << "'.\n";
+                width = height = 0;
+            }
+
+            bytes_per_scanline = bytes_per_pixel * width;
+        }
+
+        ~image_texture() {
+            delete data;
+        }
+
+        virtual color value(double u, double v, const vec3& p) const override {
+            // If we have no texture data, then return solid cyan as a debugging aid.
+            if (data == nullptr)
+                return color(0,1,1);
+
+            // Clamp input texture coordinates to [0,1] x [1,0]
+            u = clamp(u, 0.0, 1.0);
+            v = 1.0 - clamp(v, 0.0, 1.0);  // Flip V to image coordinates
+
+            auto i = static_cast<int>(u * width);
+            auto j = static_cast<int>(v * height);
+
+            // Clamp integer mapping, since actual coordinates should be less than 1.0
+            if (i >= width)  i = width-1;
+            if (j >= height) j = height-1;
+
+            const auto color_scale = 1.0 / 255.0;
+            auto pixel = data + j*bytes_per_scanline + i*bytes_per_pixel;
+
+            return color(color_scale*pixel[0], color_scale*pixel[1], color_scale*pixel[2]);
+        }
+
+    private:
+        unsigned char *data;
+        int width, height;
+        int bytes_per_scanline;
 };
 #endif
