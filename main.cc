7b5f0d534504e98ff840e5f46561decdd38f653e
Tyleryin <601576661@qq.com>
2022-03-30 17:31:19 +0800

    creat cornell box; before chapter 8

--- main.cc
+++ main.cc
@@ -123,6 +123,23 @@ hittable_list simple_light() {
     objects.add(make_shared<sphere>(point3(0,8,0), 2, difflight));//球光源
     return static_cast<hittable_list>(make_shared<bvh_node>(objects,0,1));
 }
+hittable_list cornell_box() {
+    hittable_list objects;
+
+    auto red   = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    return static_cast<hittable_list>(make_shared<bvh_node>(objects,0,1));
+}
 //main.cc
 int main() {
 
@@ -174,7 +191,6 @@ int main() {
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;
-        default:
         case 5:
             world = simple_light();
             samples_per_pixel = 400;
@@ -183,6 +199,18 @@ int main() {
             lookat = point3(0,2,0);
             vfov = 20.0;
             break;
+        default:
+        case 6:
+            world = cornell_box();
+            aspect_ratio = 1.0;
+            image_width = 600;
+            samples_per_pixel = 200;
+            background = color(0,0,0);
+            lookfrom = point3(278, 278, -800);
+            lookat = point3(278, 278, 0);
+            vfov = 40.0;
+            break;
+    
     }
     // Camera
 
